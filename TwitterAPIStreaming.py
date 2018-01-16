5fdc0aed5bdf5a28354a8e4a59303fdf00c8a249
maxwell1992 <mntshinga1992@gmail.com>
2018-01-16 17:14:54 +0200

    refactor code. add buffer list to fast process & store tweets for json data. add code to serialize buffer to json.

--- TwitterAPIStreaming.py
+++ TwitterAPIStreaming.py
@@ -1,62 +1,97 @@
 from Authenticate import Authenticate
 from MyStreamListener import *
+from SMS import SMS
+
 from socket import *
 import os
 import sys
-from http.client import IncompleteRead # Python 3
+from http.client import IncompleteRead   # Python 3
 from urllib3.exceptions import ProtocolError
 
+
 class TwitterAPIStreaming:
-    streaming_duration = 0.0
 
-    def __init__(self, apiHandle):
-        self.apiHandle = apiHandle
+    tweets_buffer = []
+    m_index = [0]
+    s_time = 0.0
+    m_count = 0
+    # average tweets per second
+    average_tweets = 0
 
-    # Map from tweets in file to SMS objects
+    def __init__(self, api_handle, output_file, duration, serialize_option, full_spritzer):
 
-    def stream_tweets(self, output_file, duration, filter_language, serialize_option, full_spritzer):
+        self.apiHandle = api_handle
+
+        self.streaming_duration = duration
+        self.output_file = output_file
+        self.duration = duration
+        self.serialize_option = serialize_option
+        self.full_spritzer = full_spritzer
+
+    def stream_tweets(self):
         
         start_time = time.time()
-        self.streaming_duration = duration
 
         print()
-        if (full_spritzer):
+
+        if self.full_spritzer:
             print("ST: Streaming full twitter spritzer (sample)")
         else:
             print("ST: Streaming duration : %s seconds" % self.streaming_duration)
         print("ST: Streaming Tweets in progress ... ")
 
-        continue_st = True
-        while continue_st :
+        continue_stream = True
+        while continue_stream:
             try:
                 # tweets_public
                 # [1] Create Listener. [2] Initialize data
-                stream_listener = MyStreamListener(tweet_messages_pb=output_file, filter_lang=filter_language,
-                                                   duration=self.streaming_duration, start_time=start_time,
-                                                   option=serialize_option, full_spritzer=full_spritzer)
+                stream_listener = MyStreamListener(tweet_messages_pb=self.output_file, duration=self.streaming_duration,
+                                                   start_time=start_time, option=self.serialize_option,
+                                                   full_spritzer=self.full_spritzer,
+                                                   t_buffer=TwitterAPIStreaming.tweets_buffer,
+                                                   me_index=TwitterAPIStreaming.m_index)
 
                 # [3] Start the stream with the specified listener class
-                my_stream = tweepy.Stream(auth=self.apiHandle.auth, listener=stream_listener)
+                my_stream = tweepy.Stream(auth=self.apiHandle.auth, listener=stream_listener, retry_time=10)
 
                 # [4] Tell the stream listener to retrieve (filter) only the twitter spritzer sample
-                MyStreamListener(my_stream.sample(), filter_lang=filter_language,
-                                 duration=self.streaming_duration, start_time=start_time, 
-                                 option=serialize_option, full_spritzer=full_spritzer)
+                MyStreamListener(my_stream.sample(), duration=self.streaming_duration, start_time=start_time,
+                                 option=self.serialize_option, full_spritzer=self.full_spritzer,
+                                 t_buffer=TwitterAPIStreaming.tweets_buffer,
+                                 me_index=TwitterAPIStreaming.m_index)
 
                 return "done"
-            except ProtocolError:
-                print ("TAPIS: ProtocolError ... pass")
-                continue
-
-            except IncompleteRead:
-                print ("TAPIS: IncompleteRead ... pass")
-                continue
 
             except tweepy.TweepError as te_error:
                 my_stream.disconnect()
-                print("Get_tweets error: ", te_error.__str__())
-            continue_st = False
-            # break
+                print("Stream tweets error : \n", te_error.__str__())
+
+            except ProtocolError as pe:
+
+                TRACE(pe.__str__() + "\nProtocolError: \n" + "time : ")
+
+                # reconnect to api stream
+                my_stream.running = True
+                print("\nReconnecting ...")
+                continue_stream = True
+                continue
+
+            except IncompleteRead as ir:
+                print("\nIncompleteRead error : \n", ir.__str__())
+                continue
+
+            except KeyboardInterrupt:
+                # allow keyboard exiting ctr+c
+                my_stream.disconnect()
+                continue_stream = False
+
+            except ConnectionError as conn_err:
+                print("\nConnection error : ", conn_err.__str__())
+                pass
+
+            except Exception as other_error:
+                print("\nOther error :", other_error)
+                pass
 
     """send message object"""
     @staticmethod
@@ -93,24 +128,6 @@ class TwitterAPIStreaming:
             print("Error send: " + s_error.__str__())
             return "file not sent!"
 
-    def reformat_json(self, filename):
-        """Correct json data format. Insert all items in a list"""
-        if os.stat(filename).st_size == 0:
-            print("file not found")
-
-        else:
-            with open(filename, "r+") as rw_file:
-                """"""
-                json_list = []
-                for item in rw_file.read().split("\n"):
-
-                    if len(item) > 0:
-                        json_list.append(json.loads(item))
-
-                rw_file.seek(0)
-                json.dump(json_list, rw_file, indent=4)
-                rw_file.truncate()
-
     @staticmethod
     def deserialize_messages(binary_file, ser_option):
         """
@@ -128,7 +145,7 @@ class TwitterAPIStreaming:
 
         # protocol buffer
         if ser_option == 1:
-            sms_list = SerializeMessage().read_from_pb(binary_file)
+            sms_list = SerializeMessage(binary_file).read_from_pb(binary_file)
 
         # json
         elif ser_option == 2:
@@ -158,6 +175,20 @@ class TwitterAPIStreaming:
         return sms_list
 
 
+def TRACE(log_text):
+    from datetime import datetime
+    t_time = time.time()
+    print("\n%s : %f\n" % (log_text, t_time))
+    try:
+        f_log = open("TRACE.txt", "a+")
+        f_log.write("\n%s : %f" % (log_text, time.time()))
+        f_log.write("\nDatetime : %s" % datetime.fromtimestamp(t_time).strftime('%c'))
+        f_log.close()
+    except Exception as err:
+        print(err.__str__())
+        print("TRACE.txt not found. New file created")
+
+
 def main():
     """ Define application key, key-secret and access-token """
     consumer_key = "g104Mx068t8JqSOfCCGPJ5xhx"
@@ -168,75 +199,83 @@ def main():
     # if len(sys.argv) != n: print "usage: python 'TwitterAPIStreaming.py 'pb filename''"
     # file = open(sys.argv[1], "rb")
     filename = "media/twitter_spritzer"
-    filter_languages = ["en"]
-    full_spritzer_i = False
+    full_spritzer = False
     option = 0
-    streaming_duration = float(0) # Enter 0 for a full spritzer
+    streaming_duration = float(60*10)   # Enter 0 for a full spritzer
 
-    """ ================ USER INPUT  ================ """
     print()
-    print("Please enter following inputs (blank for default test values)... ")
-    dest_server = input (" * File Destination Server-address : ") 
-    dest_port = input (" * File Destination Port-number : ") 
-    lang = input (" * Tweets Language Code [en, af,] seperated by comma (leave blank for 'en-english') : ") 
-
+    from SerializeMessage import SerializeMessage
     try:
-        option = \
-            input("\nChoose a serialising option (enter a number): \n 1. Protocol buffer \n 2. Json \n\nOption : ")
-        option = int(option)
-    except Exception:
-        print("Error : Option should be a number: 1 or 2")
+        """ ================ USER INPUT  ================ """
+        print("Please enter (blank for default test values)... ")
+        dest_server = input(" * File Destination Server-address : ")
+        dest_port = input(" * File Destination Port-number : ")
 
-    for lang_i in lang.split(","):
-        filter_languages.append(lang_i)
+        try:
+            option = \
+                input("\nChoose a serialising option (enter a number): \n 1. Protocol buffer \n 2. Json \n\nOption : ")
+            option = int(option)
+        except Exception:
+            print("Error : Option should be a number: 1 or 2")
 
-    if streaming_duration == float(0):
-        full_spritzer_i = True
-   
-    if dest_server == "":
-        dest_server = "localhost"
-    if dest_port == 0 or dest_port=="":
-        dest_port = 80
+        if streaming_duration == float(0):
+            full_spritzer = True
 
-    if option == 1:
-        filename += ".bin" 
-        print("... Data Serialising set to Protocol buffer.")
-    elif option == 2:
-        filename += ".json"
-        print("... Data Serialising set to Json.")
-    else:
-        print("Option not found. Please enter option 1 or 2.")
-        exit()
+        if dest_server == "":
+            dest_server = "localhost"
+            dest_port = 80
 
-    # Start clean. Delete the file if it already exists
-    try:
-        os.remove(filename)
-        # print("\n... File already exists. Deleted!")
-    except OSError:
-        print("\n[Remove]: File not found")
-        pass
+        if option == 1:
+            filename += ".bin"
+            print("... Data Serialising set to Protocol buffer.")
+        elif option == 2:
+            filename += ".json"
+            print("... Data Serialising set to Json.")
+        else:
+            print("Option not found. Please enter option 1 or 2.")
+            exit()
 
-    """ Authenticate application with the application keys """
-    apiHandle = Authenticate().authenticate_app(consumer_key, consumer_key_secret, access_token, access_token_secret)
-    query_stream = TwitterAPIStreaming(apiHandle)
+        # Start clean. Delete the file if it already exists
+        try:
+            os.remove(filename)
+            os.remove("TRACE.txt")
+        except OSError:
+            print("\n[Remove]: '%s' not found" % filename)
+            pass
 
-    """ Get tweets from Streaming API and Map tweets/SMSes to protocol buffers """
-    query_stream.stream_tweets(output_file=filename, duration=float(streaming_duration), filter_language=filter_languages,
-                                   serialize_option=option, full_spritzer=full_spritzer_i)
-    
-    print("\nAPI Streaming complete :)")
+        """ Authenticate application with the application keys """
+        api_handle = Authenticate().authenticate_app(consumer_key, consumer_key_secret,
+                                                     access_token, access_token_secret)
 
-    if option == 2:
-        query_stream.reformat_json(filename)
+        query_stream = TwitterAPIStreaming(api_handle, output_file=filename, duration=float(streaming_duration),
+                                           serialize_option=option, full_spritzer=full_spritzer)
 
-    print("\nData serialized to : ", filename)
-    print("File destination : [ Server = %s, Port = %s ]" % (dest_server, dest_port))
+        TRACE("Start Stream time")
+        query_stream.stream_tweets()
+        TRACE("End Stream time")
 
-    # """ Deserialize messages """
-    # query_stream.deserialize_messages(binary_file=filename, ser_option=option)
+        # Serialize the json buffer with tweets to json file
+        if option == 2:
+            ser_json = SerializeMessage(filename)
+            ser_json.export_json(TwitterAPIStreaming.tweets_buffer)
 
-    """ SMS transfer """
-    # query_stream.send_serialized_file(filename, d_server=dest_server, d_port=dest_port, option=option)
+        print("\n============================================")
+        print("API Streaming complete")
+        print("============================================")
+        print("Tweet Counter = %i tweet(s)" % TwitterAPIStreaming.m_index[0])
+        print("Tweets file size = %f MB" % (float(os.stat(filename).st_size)/1000000.0))
+
+        print("\nData serialized to : ", filename)
+        print("File destination : [ Server = %s, Port = %s ]" % (dest_server, dest_port))
+
+        # """ Deserialize messages """
+        # query_stream.deserialize_messages(binary_file=filename, ser_option=option)
+
+        """ SMS transfer """
+        # query_stream.send_serialized_file(filename, d_server=dest_server, d_port=dest_port, option=option)
+
+    except Exception as exp:
+        print("error : ", exp.__str__())
 
 
 if __name__ == "__main__":
